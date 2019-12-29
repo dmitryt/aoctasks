0b7b2dbb0f330871516bca766e293642d56468f4
Dmitry Tselinko <dmitry.tselinko@gmail.com>
2019-12-29 17:31:38 +0200

    2019/6

--- 2019/6.go
+++ 2019/6.go
@@ -11,9 +11,8 @@ type Vertex struct {
 	children []*Vertex;
 }
 
-func part1(content []byte) {
+func part1(orbits []string) {
 	storage := make(map[string]string)
-	orbits := strings.Split(string(content), "\n")
 	for _, orbit := range orbits {
 		parsed := strings.Split(string(orbit), ")")
 		storage[parsed[1]] = parsed[0]
@@ -36,7 +35,49 @@ func part1(content []byte) {
 	fmt.Println(result)
 }
 
+func part2(orbits []string) {
+	storage := make(map[string]string)
+	for _, orbit := range orbits {
+		parsed := strings.Split(string(orbit), ")")
+		storage[parsed[1]] = parsed[0]
+	}
+	indexOf := func(arr []string, el string) int {
+		result := -1
+		for index, item := range arr {
+			if item == el {
+				return index
+			}
+		}
+		return result
+	}
+
+	collectParents := func(node string) []string {
+		result := []string{}
+		current := node
+		for {
+			parent, ok := storage[current]
+			if !ok {
+				return result
+			}
+			result = append(result, parent)
+			current = parent
+		}
+		return result
+	}
+	left := collectParents("YOU")
+	right := collectParents("SAN")
+	result := []string{}
+	for lIndex, el := range left {
+		rIndex := indexOf(right, el)
+		if rIndex != -1 {
+			result = append(left[:lIndex + 1], right[:rIndex]...)
+			break
+		}
+	}
+	fmt.Println(len(result) - 1)
+}
+
 func main() {
 	content, _ := ioutil.ReadFile("6.txt")
-	part1(content)
+	part2(strings.Split(string(content), "\n"))
 }
